eec9ce553fbb35126b5b9947e67623da2cd51e34
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-09-08 13:07:14 -0400

    Updated candidates table

--- __tests__/db/schema..sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE candidates (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY
-    first_name VARCHAR(30) NOT NULL
-    last_name VARCHAR(30) NOT NULL
-    indutry_connected BOOLEAN NOT NULL
-);
